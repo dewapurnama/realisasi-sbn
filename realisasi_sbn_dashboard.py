383f0b4c3ff4a468b0e2e93c6893f45b14fd63bf
dewapurnama <75442383+dewapurnama@users.noreply.github.com>
2025-02-27 23:38:38 +0700

    Update realisasi_sbn_dashboard.py

--- realisasi_sbn_dashboard.py
+++ realisasi_sbn_dashboard.py
@@ -15,8 +15,8 @@ import plotly.express as px
 st.set_page_config(page_title="Realisasi SBN", page_icon=":bar_chart:",layout="wide")
 st.title(":bar_chart: Dashboard Realisasi SBN DJPPR")
 #st.markdown('<style>div.block-container{padding-top:1rem;}</style', unsafe_allow_html=True)
-data_baru = st.file_uploader("Upload Data Terbaru Disini", type=['xls', 'xlsx'])
 df_dropped=None
+data_baru = st.file_uploader("Upload Data Terbaru Disini", type=['xls', 'xlsx'])
 if data_baru is not None:
     df_baru = pd.read_excel(data_baru, sheet_name="SBN", header=2)
 
